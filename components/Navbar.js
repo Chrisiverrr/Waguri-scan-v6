7b39fbd1513f228b7a76cc20872f518013910952
Chrisiverrr <decinillachrisiver@gmail.com>
2025-08-01 19:00:57 +0800

    Create Navbar.js

---
+++ components/Navbar.js
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <nav className="bg-waguri text-white p-4 shadow">
+      <div className="max-w-6xl mx-auto font-bold text-xl">Waguri Scan</div>
+    </nav>
+  );
+    }
